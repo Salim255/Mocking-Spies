2210c4ffcb53427a6875011dca44246a361d2f42
Salim <salim.hassan.welleat@gmail.com>
2023-05-16 14:54:08 +0200

    Getting started with Mocks

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -1,8 +1,12 @@
-import { it, expect } from "vitest";
+import { it, expect, vi } from "vitest";
+import { promises as fs } from "fs";
 import writeData from "./io";
-
+vi.mock("fs");
+//This will replace all the methodes on this module with empty spie methode, that because fs replaced by empty fuction that dont write any text
 it("should excute the writeFile method", () => {
   const test = "Test";
   const testFileName = "test.text";
-  return expect(writeData(test, testFileName)).resolves.toBeUndefined();
+  writeData(test, testFileName);
+  expect(fs.writeFile).toBeCalled();
+  //return expect(writeData(test, testFileName)).resolves.toBeUndefined();
 });
